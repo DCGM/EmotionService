a1efa91b862834d480bc9e010c2dfca9c0fdd88d
Michal Hradis <ihradis@fit.vutbr.cz>
2017-03-29 14:25:28 +0200

    Download newe emotion network.

--- getNets.sh
+++ getNets.sh
@@ -5,4 +5,4 @@ rm vgg_face_caffe.tar.gz
 mkdir -p -- "./data/Nets"
 wget -O ./data/Nets/genderDEX.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/gender.caffemodel
 wget -O ./data/Nets/ageDEX.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/dex_imdb_wiki.caffemodel
-wget -O ./data/Nets/emotions.caffemodel https://www.dropbox.com/s/3dyw18e5y6m6p3i/emotions.caffemodel?dl=1
+wget -O ./data/Nets/emotionsAllNew100k.caffemodel https://www.dropbox.com/s/3dyw18e5y6m6p3i/emotionsAllNew100k.caffemodel?dl=1
