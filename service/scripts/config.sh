6eab66d4e779cfed4a68cb16d389505d230d6599
Michal Hradis <ihradis@fit.vutbr.cz>
2017-03-29 13:38:53 +0200

    Minor cleanup.

--- service/scripts/config.sh
+++ service/scripts/config.sh
@@ -3,6 +3,5 @@ SCRIPT_PATH="${ROOT_DIR}"/src
 DATA_PATH="${ROOT_DIR}"/data
 O_FACE="${ROOT_DIR}"/OpenFace/build/bin/FeatureExtraction
 MTCNN_PATH="${ROOT_DIR}"/mtcnn
---detector_path
 CNN_DETECTOR_PATH="${ROOT_DIR}"/mtcnn
 export PYTHONPATH=$PYTHONPATH:${CNN_DETECTOR_PATH}
