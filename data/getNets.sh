95bb572af2e5c9efcd6fd9cf9aa88b6fac6b1fbe
Michal Hradis <ihradis@fit.vutbr.cz>
2017-03-29 12:17:09 +0200

    Add data - emoticons and nets.

---
+++ data/getNets.sh
@@ -0,0 +1,5 @@
+mkdir -p -- "Nets"
+
+wget -O Nets/genderDEX.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/gender.caffemodel
+wget -O Nets/ageDEX.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/dex_imdb_wiki.caffemodel
+wget -O Nets/emotions.caffemodel https://www.dropbox.com/s/3dyw18e5y6m6p3i/emotions.caffemodel?dl=1
